c9574ac34ddebfb8a3a907f0561bf15e627d1555
cmahmadalitariq <57289114+cmahmadalitariq@users.noreply.github.com>
2019-11-20 23:35:48 +0500

    Add files via upload

---
+++ home.blade.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+@import url('https://fonts.googleapis.com/css?family=Titillium+Web:400,600');
+  <style>
+     .gggrid-container1 {
+  display: grid;
+   grid-gap: 20px;
+  border-color:white;
+  grid-template-columns: auto auto auto auto ;
+  grid-template-rows: auto auto  auto auto  ;
+ 
+ 
+     height:100% ;
+  width:100% ;
+  position:relative;
+ 
+}
+
+.gggrid-container1 > div {
+  background-color:white;
+   
+  text-align: center;
+  font-size: 30px;
+  width: 550px;
+  height: 800px;
+padding-left:80px;
+}
+   .grid-container1 {
+  display: grid;
+  padding-top:75px;
+  border-color:white;
+  grid-template-columns: auto auto auto auto ;
+  grid-template-rows: auto auto  auto auto  ;
+  grid-gap: 30px;
+  background-color: white;
+     height:100% ;
+  width:100% ;
+  position:relative;
+    padding-left:30px;
+}
+
+.grid-container1 > div {
+  background-color:white;
+   
+  text-align: center;
+  font-size: 30px;
+  width: 390px;
+  height: 650px;
+}
+.container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.image {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+.navbar a::before,
+.nav a::after {
+  content: '';
+  height: 14px;
+  width: 14px;
+  position: absolute;
+  transition: all .35s ease;
+  opacity: 0;
+}
+
+.navbar a::before {
+  content: '';
+  right: 0;
+  top: 0;
+  border-top: 3px solid #3E8914;
+  border-right: 3px solid #2E640F;
+  transform: translate(-100%, 50%);
+}
+
+.navbar a:after {
+  content: '';
+  left: 0;
+  bottom: 0;
+  border-bottom: 3px solid #2E640F;
+  border-left: 3px solid #3E8914;
+  transform: translate(100%, -50%)
+}
+
+.navbar a:hover:before,
+.nav a:hover:after{
+  transform: translate(0,0);
+  opacity: 1;
+}
+
+.navbar a:hover {
+  color: black;
+}
+  #navbar {
+   
+  position: fixed;
+  top: -50px;
+  width: 100%;
+  display: block;
+  transition: top 0.3s;
+}
+
+    /* Remove the navbar's default rounded borders and increase the bottom margin */ 
+    .navbar {
+      margin-bottom: 50px;
+      border-radius: 0;
+    }
+    
+    /* Remove the jumbotron's default bottom margin */ 
+     .jumbotron {
+	 background-image: url("aa.jpg");
+	 height:500px;
+	 width:100%;
+	  background-repeat: no-repeat;
+	 
+      margin-bottom: 0;
+    }
+   
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }
+	.container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+ 
+.ggrid-container1 {
+  display: grid;
+  
+  grid-template-columns: auto auto auto auto ;
+  grid-template-rows: auto auto  auto auto  ;
+  grid-gap: 20px;
+ background-color:#f0f0d0;
+     height:770px ;
+  width:100% ;
+  position:relative;
+    padding-left:255px;
+}
+.text-block {
+  position: absolute;
+  bottom: 380px;
+  right: 1px;
+  background-color: black;
+  color: white;
+  padding-left: 20px;
+  padding-right: 50px;
+ opacity: 0.8;
+  filter: alpha(opacity=50); 
+  padding:20px;
+  width:300px;
+}
+.text-blockk {
+  position: absolute;
+  bottom: 250px;
+ 
+  background-color: black;
+  color: white;
+  padding-left: 10px;
+  padding-right: 50px;
+ opacity: 0.8;
+  filter: alpha(opacity=50); 
+ 
+  width:391px;
+  height:35px;
+}
+ .txt{
+	color: #303030;
+font-size:70px;
+ padding-left:512px;
+text-shadow: 0 0 2px #fff;
+font-family: 'Lato',sans-serif;
+font-style: inherit;
+}
+ #mySidenav a {
+  position: absolute;
+  left: -80px;
+  transition: 0.3s;
+  padding: 15px;
+  width: 100px;
+  text-decoration: none;
+  font-size: 20px;
+  color: white;
+  border-radius: 0 5px 5px 0;
+}
+
+#mySidenav a:hover {
+  left: 0;
+}
+
+imgg:hover {
+  -webkit-transform: scaleX(-1); transform: scaleX(-1);
+}
+
+
+  </style>
+</head>
+<body>
+ 
+<div class="w3-content w3-section" style="max-width:100%">
+  <img class="mySlides" src="slide.png" style="width:100%">
+  <img class="mySlides" src=" slidess.png" style="width:100%">
+ 
+    <img class="mySlides" src="perr.jpg" style="width:100%">
+</div>
+<script>
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 7000); // Change image every 2 seconds
+}
+</script>
+<nav class="navbar navbar-inverse navbar-fixed-top" style="background-color:white ; ">
+  <div class="container-fluid">
+    
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav"  style="padding-left:300px;">
+	   <li><a href="#" style="  color:black;">HOME</a></li>
+	   <li><a href="#" style="  color:black;">NEW ARRIVALS</a></li>
+         <li><a href="#" style="color:black;" >WOMEN</a></li>
+        <li><a href="#" style="color:black;">MEN</a></li>
+        <li><a href="#" style="color:black;">BOYS</a></li>
+        <li><a href="#" style="color:black;">GIRLS</a></li>
+        <li><a href="fragnance.blade.php" style="color:black;" >FRAGNACE</a></li>
+		 
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="myaccount.blade.php" style="color:black;"><span class="glyphicon glyphicon-user"></span> Your Account</a></li>
+        <li><a href="#" style="color:black;"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+   <div class="ggrid-container1">
+ <div class=" container">
+ <div class=" imgg">
+   <img src="lad.jpg" alt="Avatar"   style="  width: 350px;height:450px;  display: block;  -webkit-transform: scaleX(-1); transform: scaleX(-1);" ></div>
+<div class="text-block">
+    <h4>WOMEN</h4>
+    <p>Ready to wear a dress</p>
+  </div>   
+  </div>  
+ <div class=" container">
+ 
+   <img src="ladd.jpg" alt="Avatar"   style="  width: 370px;height:650px; display: block;">   
+    </div>  
+  </div>
+  
+  <div class="ggrid-container1">
+ <div class=" container">
+ 
+   <img src="me.jpg" alt="Avatar"   style="  width: 370px;height:650px;  display: block;" >
+  
+  </div>  
+ <div class=" container">
+ 
+   <div class=" imgg"> <img src="mee.jpg" alt="Avatar"   style="  width: 370px;height:450px; display: block;">  </div> 
+<div class="text-block">6
+    <h4>MENZ WEAR</h4>
+    <p>Experience the most premium wear</p>
+  </div>    
+   </div>  
+  </div>
+ <div class="grid-container1">
+  <div>
+  
+ <div class=" container">
+  <div class="image">
+  <div  "> <div class=" imgg"><img src="md.webp" alt="Avatar"   style="width:390px;height:650px; " > </div>
+<div class="text-blockk">
+    <h4>Formal Shits</h4>
+   
+  </div>     
+  </div></div></div>  </div>
+  <div class=" container">
+  <div> <div class="image">
+  <div  "> <div class=" imgg"><img src="mdd.webp" alt="Avatar"   style="width:390px;height:650px;"> </div>
+<div class="text-blockk">
+    <h4>Suiting</h4>
+    
+  </div>     
+  </div></div></div></div>
+  <div class=" container">
+  <div><div class="image">
+  <div  ">   <!-- <img src="img/instagaram-img.jpg" alt=""> --> <div class=" imgg"><img src="mddd.webp" alt="Avatar"   style="width:390px;height:650px;"> <//div>
+<div class="text-blockk">
+    <h4>Casual Shirts</h4>
+   
+  </div>     
+  </div></div></div>  </div>
+    
+ </div>
+  <div class="gggrid-container1">
+  <div>
+  
+ <div class=" container">
+  <div class="image">
+  <div  "> <div class=" imgg"><img src="ms.jpg" alt="Avatar"   style="width:550px;height:800px;  " > </div>
+     
+  </div></div></div>  </div>
+  <div class=" container">
+  <div> <div class="image">
+  <div  "><a href="fragnance.blade.php" style="color:black;"> <div class=" imgg"><img src="ws.webp" alt="Avatar"   style="width:550px;height:800px;"> </div></a>
+     
+  </div></div></div></div> </div>
+  
+  <div class="container-fluid text-center" style=" height:150px;width:100%;color:white;background-color:black;" >
+  <p>BE The First</p>  
+   <p>Sign up to stay in the know.</p>  
+  <form class="form-inline">Get deals:
+    <input type="email" class="form-control" size="50" placeholder="Email Address">
+    <button type="button" class="btn btn-danger" style="border: 2px solid white; background-color:black;">SIGN UP</button>
+  </form>
+</div>
+
+</body>
+</html>
